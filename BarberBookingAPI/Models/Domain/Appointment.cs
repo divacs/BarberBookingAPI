808fea398ac0d79aa0569d5e331a5301f831414d
divacs <sonja.divac@yahoo.com>
2025-05-27 15:56:30 +0200

    added domain models

---
+++ BarberBookingAPI/Models/Domain/Appointment.cs
@@ -0,0 +1,14 @@
+﻿namespace BarberBookingAPI.Models.Domain
+{
+    public class Appointment
+    {
+        public int Id { get; set; } // Unique identifier for the appointment, using an integer type
+        public DateTime StartTime { get; set; } // Start time of the appointment
+        public DateTime EndTime { get; set; } // End time of the appointment
+        public string ClientId { get; set; } // Identifier for the client, typically a user ID
+        public ApplicationUser Client { get; set; } // Navigation property for the client
+        public string WorkerId { get; set; } // Identifier for the worker (barber), typically a user ID
+        public string BarberServiceId { get; set; } // Identifier for the barber service
+        public BarberService BarberService { get; set; } // Navigation property for the barber service
+    }
+}
---
+++ BarberBookingAPI/Models/Domain/BarberService.cs
@@ -0,0 +1,12 @@
+﻿namespace BarberBookingAPI.Models.Domain
+{
+    public class BarberService
+    {
+        public string Id { get; set; } // Unique identifier for the barber service, using a string type
+        public string Name { get; set; } // Name of the barber service
+        public int Duration { get; set; } // Duration of the service in minutes
+        public int Price { get; set; } // Price of the service in the smallest currency unit (e.g., rsd)
+        public ICollection<Appointment> Appointments { get; set; } // Navigation property for appointments
+
+    }
+}
