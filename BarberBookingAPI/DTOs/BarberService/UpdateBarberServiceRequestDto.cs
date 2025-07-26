68e7de4e09413c637a46982fa2fe164737392c83
divacs <sonja.divac@yahoo.com>
2025-07-26 23:05:58 +0200

    dtos needed for barberService

--- BarberBookingAPI/DTOs/BarberService/UpdateBarberServiceRequestDto.cs
+++ BarberBookingAPI/DTOs/BarberService/UpdateBarberServiceRequestDto.cs
@@ -1,8 +1,7 @@
 ﻿namespace BarberBookingAPI.DTOs.BarberService
 {
-    public class BarberServiceDto
+    public class UpdateBarberServiceRequestDto
     {
-        public int Id { get; set; } // Unique identifier for the barber service, using a string type
         public string Name { get; set; } = string.Empty; // Name of the barber service
         public int Duration { get; set; } // Duration of the service in minutes
         public int Price { get; set; } // Price of the service in the smallest currency unit (e.g., rsd)
