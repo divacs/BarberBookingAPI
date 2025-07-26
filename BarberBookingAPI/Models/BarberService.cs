555fa94d6e611ddd361d6276e1b17fd98ba02d18
divacs <sonja.divac@yahoo.com>
2025-07-26 23:05:28 +0200

    mode fix barberService

--- BarberBookingAPI/Models/BarberService.cs
+++ BarberBookingAPI/Models/BarberService.cs
@@ -2,7 +2,7 @@
 {
     public class BarberService
     {
-        public string Id { get; set; } // Unique identifier for the barber service, using a string type
+        public int Id { get; set; } // Unique identifier for the barber service, using a string type
         public string Name { get; set; } = string.Empty; // Name of the barber service
         public int Duration { get; set; } // Duration of the service in minutes
         public int Price { get; set; } // Price of the service in the smallest currency unit (e.g., rsd)
