b0bcb69178ab44e98970b772015c5cb4c1a99ff6
divacs <sonja.divac@yahoo.com>
2025-07-26 21:22:41 +0200

    added interface for appointment

---
+++ BarberBookingAPI/Interfaces/IAppointmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using BarberBookingAPI.DTOs.Apointment;
+using BarberBookingAPI.Models;
+
+namespace BarberBookingAPI.Interfaces
+{
+    public interface IAppointmentRepository
+    {
+        Task<List<Appointment>> GetAllAsnc();
+        Task<Appointment?> GetByIdAsync(int id);
+        Task<Appointment> CreateAsync (Appointment appointmentModel);
+        Task<Appointment?> UpdateAsync(int id, UpdateAppointmentRequestDto appointmentDto);
+        Task<Appointment?> DeleteAsync(int id);
+    }
+}
