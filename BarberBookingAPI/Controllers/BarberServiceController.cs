615b9a921de3c0c8349cd81039b955b970fda770
divacs <sonja.divac@yahoo.com>
2025-07-26 21:58:24 +0200

    get all in contrroler for barberService

---
+++ BarberBookingAPI/Controllers/BarberServiceController.cs
@@ -0,0 +1,27 @@
+﻿using BarberBookingAPI.Interfaces;
+using BarberBookingAPI.Mapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarberBookingAPI.Controllers
+{
+    [Route("api/barberService")]
+    [ApiController]
+    public class BarberServiceController : ControllerBase
+    {
+        private readonly IBarberServiceRepository _barberServiceRepo;
+        public BarberServiceController(IBarberServiceRepository barberServiceRepo)
+        {
+            _barberServiceRepo = barberServiceRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var barberService = await _barberServiceRepo.GetAllAsync();
+
+            var barberServiceDto = barberService.Select(b => b.ToBarberServiceDto());
+
+            return Ok(barberService);
+        }
+    }
+}
