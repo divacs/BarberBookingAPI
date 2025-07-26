5685184afeb75886a14326b1fde01df3eb1ed782
divacs <sonja.divac@yahoo.com>
2025-07-26 22:18:47 +0200

    added methods for barberService

--- BarberBookingAPI/Interfaces/IBarberServiceRepository.cs
+++ BarberBookingAPI/Interfaces/IBarberServiceRepository.cs
@@ -5,5 +5,9 @@ namespace BarberBookingAPI.Interfaces
     public interface IBarberServiceRepository
     {
         Task<List<BarberService>> GetAllAsync();
+        Task<BarberService?> GetByIdAsync(int id);
+        Task<BarberService> CreateBarberService(BarberService barberService);
+        Task<BarberService?> UpdateByIdAsync(int id, BarberService barberService);
+        Task<BarberService?> DeleteByServiceAsync(int id);
     }
 }
--- BarberBookingAPI/Repository/BarberServiceRepository.cs
+++ BarberBookingAPI/Repository/BarberServiceRepository.cs
@@ -1,4 +1,6 @@
 ﻿using BarberBookingAPI.Data;
+using BarberBookingAPI.DTOs.Apointment;
+using BarberBookingAPI.DTOs.BarberService;
 using BarberBookingAPI.Interfaces;
 using BarberBookingAPI.Models;
 using Microsoft.EntityFrameworkCore;
@@ -12,9 +14,50 @@ namespace BarberBookingAPI.Repository
         {
             _context = context;
         }
+
+        public async Task<BarberService> CreateBarberService(BarberService barberServiceModel)
+        {
+            await _context.BarberServices.AddAsync(barberServiceModel);
+            await _context.SaveChangesAsync();
+            return barberServiceModel;
+        }
+
+        public async Task<BarberService?> DeleteByServiceAsync(int id)
+        {
+            var barberServiceModel = await _context.BarberServices.FindAsync(id);
+
+            if (barberServiceModel == null)
+            {
+                return null;
+            }
+
+            _context.BarberServices.Remove(barberServiceModel);
+            await _context.SaveChangesAsync();
+            return barberServiceModel;
+        }
+
         public async Task<List<BarberService>> GetAllAsync()
         {
             return await _context.BarberServices.ToListAsync();
         }
+
+        public async Task<BarberService?> GetByIdAsync(int id)
+        {
+            return await _context.BarberServices.FindAsync(id);
+        }
+
+        public async Task<BarberService?> UpdateByIdAsync(int id, BarberService barberServiceModel)
+        {
+            var existingBarberService = await _context.BarberServices.FindAsync(id);
+
+            existingBarberService.Name = barberServiceModel.Name;
+            existingBarberService.Duration = barberServiceModel.Duration;
+            existingBarberService.Price = barberServiceModel.Price;
+
+            await _context.SaveChangesAsync();
+
+            return existingBarberService;
+        }
+
     }
 }
