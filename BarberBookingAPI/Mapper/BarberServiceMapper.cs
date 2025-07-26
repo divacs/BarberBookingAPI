c2a16f42d070a76a35abf9d69e2180533bbccc1f
divacs <sonja.divac@yahoo.com>
2025-07-26 21:58:06 +0200

    mapper for barberService

---
+++ BarberBookingAPI/Mapper/BarberServiceMapper.cs
@@ -0,0 +1,22 @@
+﻿using BarberBookingAPI.DTOs.Apointment;
+using BarberBookingAPI.DTOs.BarberService;
+using BarberBookingAPI.Models;
+using System.Runtime.CompilerServices;
+
+namespace BarberBookingAPI.Mapper
+{
+    public static class BarberServiceMapper
+    {
+        public static BarberServiceDto ToBarberServiceDto(this BarberService barberServiceModel)
+        {
+            return new BarberServiceDto
+            {
+                Id = barberServiceModel.Id,
+                Name = barberServiceModel.Name,
+                Duration = barberServiceModel.Duration,
+                Price = barberServiceModel.Price,
+
+            };
+        }
+    }
+}
