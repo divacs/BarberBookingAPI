9feb13e07bf4306814c279af0afcf92fe4e77717
divacs <sonja.divac@yahoo.com>
2025-07-28 16:13:07 +0200

    added data antotation

--- BarberBookingAPI/DTOs/BarberService/CreateBarberServiceRequestDto.cs
+++ BarberBookingAPI/DTOs/BarberService/CreateBarberServiceRequestDto.cs
@@ -1,9 +1,15 @@
-﻿namespace BarberBookingAPI.DTOs.BarberService
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BarberBookingAPI.DTOs.BarberService
 {
     public class CreateBarberServiceRequestDto
     {
+        [Required]
+        [MinLength(5, ErrorMessage = "Name must be 3 letters or more")]
         public string Name { get; set; } = string.Empty; // Name of the barber service
+        [Required]
         public int Duration { get; set; } // Duration of the service in minutes
+        [Required]
         public int Price { get; set; } // Price of the service in the smallest currency unit (e.g., rsd)
     }
 }
