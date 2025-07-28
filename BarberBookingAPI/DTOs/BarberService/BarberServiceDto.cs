b837751140f8a76de1135c881822441e9a9f941b
divacs <sonja.divac@yahoo.com>
2025-07-28 16:11:53 +0200

    added data antotation for min lenght name

--- BarberBookingAPI/DTOs/BarberService/BarberServiceDto.cs
+++ BarberBookingAPI/DTOs/BarberService/BarberServiceDto.cs
@@ -8,6 +8,7 @@ namespace BarberBookingAPI.DTOs.BarberService
         [Required]
         public string Name { get; set; } = string.Empty; // Name of the barber service
         [Required]
+        [MinLength(5, ErrorMessage = "Name must be 3 letters or more")]
         public int Duration { get; set; } // Duration of the service in minutes
         [Required]
         public int Price { get; set; } // Price of the service in the smallest currency unit (e.g., rsd)
