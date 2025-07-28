37587d9c8f2fd8415f4925a2523914a2e14ebdfd
divacs <sonja.divac@yahoo.com>
2025-07-28 16:10:04 +0200

    added data antotation for barberServiceDto

--- BarberBookingAPI/DTOs/BarberService/BarberServiceDto.cs
+++ BarberBookingAPI/DTOs/BarberService/BarberServiceDto.cs
@@ -1,10 +1,15 @@
-﻿namespace BarberBookingAPI.DTOs.BarberService
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BarberBookingAPI.DTOs.BarberService
 {
     public class BarberServiceDto
     {
         public int Id { get; set; } // Unique identifier for the barber service, using a string type
+        [Required]
         public string Name { get; set; } = string.Empty; // Name of the barber service
+        [Required]
         public int Duration { get; set; } // Duration of the service in minutes
+        [Required]
         public int Price { get; set; } // Price of the service in the smallest currency unit (e.g., rsd)
     }
 }
