508ee5c299cf43185e3a1d22df8f9cd6284f7d6f
divacs <sonja.divac@yahoo.com>
2025-07-29 16:46:23 +0200

    script for killing process

---
+++ BarberBookingAPI/Kill-BarberApi.ps1
@@ -0,0 +1,8 @@
+﻿$process = Get-Process BarberBookingAPI -ErrorAction SilentlyContinue
+
+if ($process) {
+    Stop-Process -Name "BarberBookingAPI"
+    Write-Output "Process BarberBookingAPI stopped."
+} else {
+    Write-Output "No running process named BarberBookingAPI found."
+}
