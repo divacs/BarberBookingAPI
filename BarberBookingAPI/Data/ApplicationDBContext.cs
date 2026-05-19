4eb91a5cfa99b604c32210e4ec637487faf68012
divacs <sonja.divac@yahoo.com>
2026-05-19 21:20:35 +0200

    fix: stabilize identity role seed ids

--- BarberBookingAPI/Data/ApplicationDBContext.cs
+++ BarberBookingAPI/Data/ApplicationDBContext.cs
@@ -8,6 +8,10 @@ namespace BarberBookingAPI.Data
 {
     public class ApplicationDBContext : IdentityDbContext<ApplicationUser> // Inherits from IdentityDbContext to support ASP.NET Core Identity
     {
+        private const string AdminRoleId = "3bfa9a6e-4897-4395-acd7-0b1ba741e474";
+        private const string UserRoleId = "d598eac7-55ec-4862-8d53-356cb52d7a64";
+        private const string WorkerRoleId = "5ae5c4c0-ff98-407b-8b99-94ca20af6b2f";
+
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> dbContextOptions) : base(dbContextOptions)
         {
         }
@@ -23,16 +27,19 @@ namespace BarberBookingAPI.Data
             {
                 new IdentityRole
                 {
+                    Id = AdminRoleId,
                     Name = "Admin",
                     NormalizedName = "ADMIN"
                 },
                 new IdentityRole
                 {
+                    Id = UserRoleId,
                     Name = "User",
                     NormalizedName = "USER"
                 },
                 new IdentityRole
                 {
+                    Id = WorkerRoleId,
                     Name = "Worker",
                     NormalizedName = "WORKER"
                 },
